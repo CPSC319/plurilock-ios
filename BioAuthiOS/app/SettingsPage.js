6db3789af7613dc3503490fa8344945691a9f99f
Bruce Li <bruce.li@zenchatapp.com>
2016-03-01 11:53:09 -0800

    UI for overview page and added text box to settings

--- BioAuthiOS/app/SettingsPage.js
+++ BioAuthiOS/app/SettingsPage.js
@@ -6,7 +6,8 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput
 } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -20,14 +21,54 @@ const styles = StyleSheet.create({
   }
 })
 
+import {GestureLogger} from 'NativeModules'
+
 export default class SettingsPage extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ""
+    };
+
+    this.onTextChange = this.onTextChange.bind(this)
+  }
+
 
   render() {
     return(
       <View>
       <Text style={styles.text}>SETTINGS PAGE</Text>
+
+      <TextInput
+                  style={{marginTop: 64, height: 60, borderColor: 'gray', borderWidth: 1}}
+                  onChangeText={this.onTextChange}
+                  value={this.state.text}
+      />
       </View>
     );
   }
+
+
+      onTextChange(text) {
+
+        if (text.length > this.state.text.length) {
+          var keyAddedToEnd = true
+          for (var i = 0; i< this.state.text.length; i++) {
+            if (text[i] != this.state.text[i]) {
+              GestureLogger.retrieveKeyData("BioAuthiOS", new Date().toString(), text[i])
+              keyAddedToEnd = false
+            }
+          }
+
+          if (keyAddedToEnd) {
+            GestureLogger.retrieveKeyData("BioAuthiOS", new Date().toString(), text[text.length-1])
+          }
+
+        }
+
+        this.setState({text})
+      }
 
 }
