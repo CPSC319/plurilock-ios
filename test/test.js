f11310cb4427b39faa7e8a8358d00c1979c4ae31
William Qi <wqi@lai.io>
2016-04-07 02:32:52 -0700

    add additional dependencies

--- test/test.js
+++ test/test.js
@@ -1,9 +1,34 @@
 var assert = require('assert');
+var WebSocket = require('ws');
+var expect = require('chai').expect;
+
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
+
+describe('Server Connection', function() {
+  describe('Client', function () {
+    it('should be able to connect to the Plurilock server', function () {
+      var ws = new WebSocket('ws://btdemo.plurilock.com:8095');
+
+      ws.ping();
+    });
+    it('should be able to send stringifed data to the Plurilock server', function () {
+      //    var data = {
+      //     "btClientType": "iOS",
+      //     "btClientVersion":"1.0",
+      //     "userID":"Bruce",
+      //     "domain":"team2",
+      //     "data":callback
+        
+      // ws.on('open', function open() {
+      //   ws.send('something');
+      // });
     });
   });
 });
